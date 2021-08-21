35a7688fdfade6f0fddc3188ff44177021ee452e
gavinquach <gvqch8@gmail.com>
2021-08-22 01:24:53 +0700

    added list all available items on home page for debug

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import AuthService from "./services/auth.service";
+
 import NavigationBar from "./NavigationBar";
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { items: [] };
+    }
+
+    load = () => {
+        AuthService.viewAllItems().then(response => {
+            // console.log(response.data);
+            this.setState({ items: response.data }, () => console.log(this.state.items));
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    componentDidMount() {
+        this.load();
+    }
+
     render() {
         return (
             <div>
                 <NavigationBar />
                 <div className="container">
-                    <header className="jumbotron">
-                        <h3>Hello</h3>
-                    </header>
+                    <h1>Hello</h1>
+                    {this.state.items.map(item =>
+                        <a href={"item/" + item._id}>
+                            <div className="ItemPanel">
+                                {item.images.map(image =>
+                                    image.cover && (
+                                        <img src={Buffer.from(image.data_url).toString('utf8')} alt={image.name} />
+                                    )
+                                )}
+                                <h4>{item.name} for {item.forItemName}</h4>
+                            </div>
+                        </a>
+                    )}
                 </div>
-            </div>
+            </div >
         );
     }
 }
