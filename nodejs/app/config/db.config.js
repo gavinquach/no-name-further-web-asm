d9a81f410994bebece77e564ea4c86b05ab68068
gavinquach <gvqch8@gmail.com>
2021-08-22 22:38:27 +0700

    new mongoDB region (singapore)

--- nodejs/app/config/db.config.js
+++ nodejs/app/config/db.config.js
@@ -4,5 +4,5 @@ module.exports = {
     HOST: "localhost",
     PORT: 27017,
     DB: "no_name_db",
-    CLOUD_DB: `mongodb+srv://no-name-member:Member0Name@cluster0.lvi8f.mongodb.net/no_name_db`
+    CLOUD_DB: `mongodb+srv://no-name-member:Member0Name@trading-food-system.lvi8f.mongodb.net/no_name_db`
 };
