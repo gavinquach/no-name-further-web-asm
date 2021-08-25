d63b79d1d8187cbca2eac0e161bbaa4ffaad8a52
gavinquach <gvqch8@gmail.com>
2021-08-25 12:11:03 +0700

    added item category validation

--- nodejs/app/controllers/auth.controller.js
+++ nodejs/app/controllers/auth.controller.js
@@ -332,156 +332,197 @@ exports.createItem = (req, res) => {
         if (err) return res.status(500).send({ message: err });
         if (!user) return res.status(404).send({ message: "User not found." });
 
-        const date = new Date();
-
-        // create item object from array passed through
-        const item = new Item({
-            name: req.body.name,
-            quantity: req.body.quantity,
-            type: req.body.type,
-            images: [],
-            forItemName: req.body.forItemName,
-            forItemQty: req.body.forItemQty,
-            forItemType: req.body.forItemType,
-            seller: user._id,
-            upload_date: date
-        });
-
-        // validate files
-        if (req.files.length <= 0) {
-            return res.status(401).send("You must upload at least 1 file.");
-        }
-        if (req.files == undefined) {
-            return res.status(404).send({ message: "Incorrect file type or file not found" });
-        }
-
-        // create list of image objects from the array passed through
-        const images = [];
-        req.files.map((image, index) => {
-            images.push(new Image({
-                name: image.filename,
-                size: image.size,
-                type: image.mimetype,
-                item: item._id,
-                cover: req.body.coverIndexes[index]
-            }));
-        })
-
-        // add images to database
-        images.map(image => {
-            image.save(err => {
-                if (err) return res.status(500).send({ message: err });
-            });
-            // add image id to item's list of images
-            item.images.push(image._id);
-        })
-
-        // add item to database
-        item.save(err => {
+        ItemCategory.find().exec((err, categories) => {
             if (err) return res.status(500).send({ message: err });
+            if (!categories) return res.status(404).send({ message: "Item categories not found!" });
 
-            user.items.push(item._id);
-            user.save(err => {
-                if (err) return res.status(500).send({ message: err });
+            let itemType = null;
+            let forItemType = null;
+            categories.map(category => {
+                if (category.name === req.body.type) {
+                    itemType = category._id;
+                } else if (category.name === req.body.forItemType) {
+                    forItemType = category._id;
+                }
             });
 
-            res.send({ message: "Item created successfully!" });
+            if (itemType === null || forItemType === null) {
+                return res.status(404).send({ message: "Invalid item category." });
+            }
+
+            // create item object from array passed through
+            const date = new Date();
+            const item = new Item({
+                name: req.body.name,
+                quantity: req.body.quantity,
+                type: itemType,
+                images: [],
+                forItemName: req.body.forItemName,
+                forItemQty: req.body.forItemQty,
+                forItemType: forItemType,
+                seller: user._id,
+                upload_date: date
+            });
+
+            // validate files
+            if (req.files.length <= 0) {
+                return res.status(401).send("You must upload at least 1 file.");
+            }
+            if (req.files == undefined) {
+                return res.status(404).send({ message: "Incorrect file type or file not found" });
+            }
+
+            // create list of image objects from the array passed through
+            const images = [];
+            req.files.map((image, index) => {
+                images.push(new Image({
+                    name: image.filename,
+                    size: image.size,
+                    type: image.mimetype,
+                    item: item._id,
+                    cover: req.body.coverIndexes[index]
+                }));
+            })
+
+            // add images to database
+            images.map(image => {
+                image.save(err => {
+                    if (err) return res.status(500).send({ message: err });
+                });
+                // add image id to item's list of images
+                item.images.push(image._id);
+            })
+
+            // add item to database
+            item.save(err => {
+                if (err) return res.status(500).send({ message: err });
+
+                user.items.push(item._id);
+                user.save(err => {
+                    if (err) return res.status(500).send({ message: err });
+                });
+
+                res.send({ message: "Item created successfully!" });
+            });
         });
     });
 };
 
 exports.editItem = (req, res) => {
-    Item.findById(req.params.id, function (err, item) {
-        if (err) return res.status(500).send({ message: err });
-        if (!item) return res.status(404).send({ message: "Item not found!" });
+    Item.findById(req.params.id)
+        .populate("type", "-__v")
+        .populate("forItemType", "-__v")
+        .exec(function (err, item) {
+            if (err) return res.status(500).send({ message: err });
+            if (!item) return res.status(404).send({ message: "Item not found!" });
 
-        // update item data
-        const itemObj = JSON.parse(req.body.item);
-        item.name = itemObj.name;
-        item.quantity = itemObj.quantity;
-        item.type = itemObj.type;
-        item.forItemName = itemObj.forItemName;
-        item.forItemQty = itemObj.forItemQty;
-        item.forItemType = itemObj.forItemType;
+            const itemObj = JSON.parse(req.body.item);
 
-        // validate files
-        if (req.files == undefined) {
-            return res.status(404).send({ message: "Incorrect file type or file not found" });
-        }
+            ItemCategory.find().exec((err, categories) => {
+                if (err) return res.status(500).send({ message: err });
+                if (!categories) return res.status(404).send({ message: "Item categories not found!" });
 
-
-        // create list of image objects
-        const images = [];
-        req.files.map((image, index) => {
-            images.push(new Image({
-                name: image.filename,
-                size: image.size,
-                type: image.mimetype,
-                item: item._id,
-                cover: JSON.parse(req.body.coverIndexes)[index]
-            }));
-        })
-
-        // remove old images from database and files
-        if (req.body.oldImages) {
-            if (Array.isArray(req.body.oldImages)) {
-                req.body.oldImages.map(oldImage => {
-                    const parsedImage = JSON.parse(oldImage);
-                    
-                    const path = img.path.concat(parsedImage.name)
-                    fs.unlink(path, (err) => {
-                        // if (err) return res.status(500).send({ message: err });
-                    });
-    
-                    // remove images from item database
-                    const imageIndex = item.images.indexOf(parsedImage._id);
-                    if (imageIndex > -1) {
-                        item.images.splice(imageIndex, 1);
+                let itemType = null;
+                let forItemType = null;
+                categories.map(category => {
+                    if (category.name === itemObj.type) {
+                        itemType = category._id;
+                    } else if (category.name === itemObj.forItemType) {
+                        forItemType = category._id;
                     }
-                    
-                    Image.deleteOne({ _id: parsedImage._id }, function (err, image) {
-                        // if (err) return res.status(500).send({ message: err });
-                        // if (!image) return res.status(404).send({ message: "Image not found." });
-                    });
                 });
-            } else {
-                const parsedImage = JSON.parse(req.body.oldImages);
-                
-                const path = img.path.concat(parsedImage.name)
-                fs.unlink(path, (err) => {
-                    // if (err) return res.status(500).send({ message: err });
+
+                if (itemType === null || forItemType === null) {
+                    return res.status(404).send({ message: "Invalid item category." });
+                }
+
+                // update item data
+                item.name = itemObj.name;
+                item.quantity = itemObj.quantity;
+                item.type = itemType;
+                item.forItemName = itemObj.forItemName;
+                item.forItemQty = itemObj.forItemQty;
+                item.forItemType = forItemType;
+
+                // validate files
+                if (req.files == undefined) {
+                    return res.status(404).send({ message: "Incorrect file type or file not found" });
+                }
+
+
+                // create list of image objects
+                const images = [];
+                req.files.map((image, index) => {
+                    images.push(new Image({
+                        name: image.filename,
+                        size: image.size,
+                        type: image.mimetype,
+                        item: item._id,
+                        cover: JSON.parse(req.body.coverIndexes)[index]
+                    }));
                 })
 
-                // remove images from item database
-                const imageIndex = item.images.indexOf(parsedImage._id);
-                if (imageIndex > -1) {
-                    item.images.splice(imageIndex, 1);
+                // remove old images from database and files
+                if (req.body.oldImages) {
+                    if (Array.isArray(req.body.oldImages)) {
+                        req.body.oldImages.map(oldImage => {
+                            const parsedImage = JSON.parse(oldImage);
+
+                            const path = img.path.concat(parsedImage.name)
+                            fs.unlink(path, (err) => {
+                                // if (err) return res.status(500).send({ message: err });
+                            });
+
+                            // remove images from item database
+                            const imageIndex = item.images.indexOf(parsedImage._id);
+                            if (imageIndex > -1) {
+                                item.images.splice(imageIndex, 1);
+                            }
+
+                            Image.deleteOne({ _id: parsedImage._id }, function (err, image) {
+                                // if (err) return res.status(500).send({ message: err });
+                                // if (!image) return res.status(404).send({ message: "Image not found." });
+                            });
+                        });
+                    } else {
+                        const parsedImage = JSON.parse(req.body.oldImages);
+
+                        const path = img.path.concat(parsedImage.name)
+                        fs.unlink(path, (err) => {
+                            // if (err) return res.status(500).send({ message: err });
+                        })
+
+                        // remove images from item database
+                        const imageIndex = item.images.indexOf(parsedImage._id);
+                        if (imageIndex > -1) {
+                            item.images.splice(imageIndex, 1);
+                        }
+
+                        Image.deleteOne({ _id: parsedImage._id }, function (err, image) {
+                            // if (err) return res.status(500).send({ message: err });
+                            // if (!image) return res.status(404).send({ message: "Image not found." });
+                        });
+                    }
                 }
-                
-                Image.deleteOne({ _id: parsedImage._id }, function (err, image) {
-                    // if (err) return res.status(500).send({ message: err });
-                    // if (!image) return res.status(404).send({ message: "Image not found." });
+
+                // add new images to database
+                images.map(image => {
+                    image.save(err => {
+                        if (err) {
+                            // return res.status(500).send({ message: err })
+                        };
+                    });
+                    // add image id to item's list of images
+                    item.images.push(image._id);
                 });
-            }
-        }
 
-        // add new images to database
-        images.map(image => {
-            image.save(err => {
-                if (err) {
-                    // return res.status(500).send({ message: err })
-                };
+                // update item in database
+                item.save(err => {
+                    if (err) return res.status(500).send({ message: err });
+                    res.send({ message: "Item updated successfully!" });
+                });
             });
-            // add image id to item's list of images
-            item.images.push(image._id);
         });
-
-        // update item in database
-        item.save(err => {
-            if (err) return res.status(500).send({ message: err });
-            res.send({ message: "Item updated successfully!" });
-        });
-    });
 };
 
 exports.deleteItem = (req, res) => {
