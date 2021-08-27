dd9dcf23c4b96905c203ecb36670ca092f71d670
gavinquach <gvqch8@gmail.com>
2021-08-27 22:48:22 +0700

    controller functions code refactor (avoid callback hell)

--- nodejs/app/controllers/transaction.controller.js
+++ nodejs/app/controllers/transaction.controller.js
@@ -85,105 +85,121 @@ exports.getItemTransactions = async (req, res) => {
 // Post a new transaction
 exports.createTransaction = async (req, res) => {
     // check if user same item in transaction and is ongoing
-    Transaction.findOne({
-        item: req.body.itemid,
-        user_buyer: req.body.userid,
+    try {
+        const transaction = await Transaction.findOne({
+            item: req.body.itemid,
+            user_buyer: req.body.userid,
+            status: "Pending"
+        }).exec();
+
+        if (transaction) return res.status(401).send({ message: "Transaction already exists!" });
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+
+    // find user and item in database to see if it exists
+    let user = null;
+    try {
+        user = await User.findById(req.body.userid)
+            .exec();
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+    if (!user) return res.status(404).send({ message: "User not found." });
+
+    // if item is found in cart, remove it
+    const itemIndexInCart = user.cart.indexOf(req.body.itemid);
+    if (itemIndexInCart > -1) user.cart.splice(itemIndexInCart, 1);
+
+    // add transaction to database
+    try {
+        await user.save();
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+
+    let item = null;
+    try {
+        item = await Item.findById(req.body.itemid).exec();
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+    if (!item) return res.status(404).send({ message: "Item not found." });
+
+    const currentDate = new Date();
+    let datePlus2Weeks = new Date();
+    datePlus2Weeks.setDate(datePlus2Weeks.getDate() + 2 * 7);   // add 2 weeks to date
+
+    // create transaction object
+    const transaction = new Transaction({
+        user_seller: item.seller,
+        user_buyer: user._id,
+        item: item._id,
+        created_date: currentDate,
+        expirational_date: datePlus2Weeks,
         status: "Pending"
-    }).exec((err, transc) => {
-        if (err) return res.status(500).send({ message: err });
-        if (transc) return res.status(401).send({ message: "Transaction already exists!" });
-
-        // find user and item in database to see if it exists
-        User.findById(req.body.userid)
-            .exec((err, user) => {
-                if (err) return res.status(500).send({ message: err });
-                if (!user) return res.status(404).send({ message: "User not found." });
-
-                // if item is found in cart, remove it
-                const itemIndexInCart = user.cart.indexOf(req.body.itemid);
-                if (itemIndexInCart > -1) user.cart.splice(itemIndexInCart, 1);
-
-                user.save(err => {
-                    if (err) return res.status(500).send({ message: err });
-                });
-
-                Item.findById(req.body.itemid)
-                    .exec((err, item) => {
-                        if (err) return res.status(500).send({ message: err });
-                        if (!item) return res.status(404).send({ message: "Item not found." });
-
-                        const currentDate = new Date();
-                        let datePlus2Weeks = new Date();
-                        datePlus2Weeks.setDate(datePlus2Weeks.getDate() + 2 * 7);   // add 2 weeks to date
-
-                        // create transaction object
-                        const transc = new Transaction({
-                            user_seller: item.seller,
-                            user_buyer: user._id,
-                            item: item._id,
-                            created_date: currentDate,
-                            expirational_date: datePlus2Weeks,
-                            status: "Pending"
-                        });
-
-                        // add transaction to database
-                        transc.save(err => {
-                            if (err) return res.status(500).send({ message: err });
-                            res.send({ message: "Transaction created successfully!" });
-                        });
-                    });
-            });
     });
+
+    // add transaction to database
+    try {
+        await transaction.save();
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+    res.status(200).send({ message: "Transaction created successfully!" });
 }
 
 // Delete transaction
 exports.deleteTransaction = async (req, res) => {
-    const transcId = req.params.id;
-    Transaction.findById(transcId, function (err, transaction) {
+    Transaction.deleteOne({
+        _id: req.params.id
+    }, (err, deleted) => {
         if (err) return res.status(500).send({ message: err });
-        if (!transaction) return res.status(404).send({ message: "Transaction not found." });
-
-        Transaction.findByIdAndRemove({
-            _id: transcId
-        }, function (err, transaction) {
-            if (err) return res.status(500).send({ message: err });
-            res.json('Transaction successfully removed');
-        });
+        if (deleted) res.status(200).send({ message: "Transaction successfully removed" });
     });
 }
 
 // Cancel transaction, set status to cancelled
 exports.cancelTransaction = async (req, res) => {
-    Transaction.findOne({
-        user_buyer: req.body.userid,
-        item: req.body.itemid,
-        status: "Pending"
-    }, function (err, transaction) {
-        if (err) return res.status(500).send({ message: err });
-        if (!transaction) return res.status(401).send({ message: "Transaction not found." });
+    let transaction = null;
+    try {
+        transaction = await Transaction.findOne({
+            user_buyer: req.body.userid,
+            item: req.body.itemid,
+            status: "Pending"
+        });
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+    if (!transaction) return res.status(401).send({ message: "Transaction not found." });
 
-        transaction.status = "Cancelled";
-        transaction.save(err => {
-            if (err) return res.status(500).send({ message: err });
-            res.send({ message: "Transaction cancelled successfully!" });
-        })
-    });
+    transaction.status = "Cancelled";
+    try {
+        await transaction.save();
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+    res.status(200).send({ message: "Transaction cancelled successfully!" });
 }
 
-// Edit to finalize
-exports.editToFinalize = async (req, res) => {
-    const date = new Date();
-    Transaction.findById(req.params.id, function (err, transc) {
-        // create finalized transaction object from array passed through
-        transc.user_buyer = req.body.transcObj.user_buyer; // Add the buyer to transaction
-        transc.finalization_date = date;
-        transc.status = "Finalized"
+// Edit transaction status to "Done"
+exports.completeTransaction = async (req, res) => {
+    let transaction = null;
+    try {
+        transaction = await Transaction.findById(req.params.id);
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+    if (!transaction) return res.status(401).send({ message: "Transaction not found." });
+    
+    // add the buyer to transaction
+    transaction.finalization_date = new Date();
+    transaction.status = "Done";
 
-        // update item in database
-        item.save(err => {
-            if (err) return res.status(500).send({ message: err });
-            res.send({ message: "Item updated successfully!" });
-        });
+    // update item in database
+    transaction.save(err => {
+        if (err) return res.status(500).send({ message: err });
+        res.status(200).send({ message: "Transaction completed!" });
     });
 };
 
