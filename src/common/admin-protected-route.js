edf6506352ee44be5508e1732e2c0f0fc3d4be87
gavinquach <gvqch8@gmail.com>
2021-08-28 23:20:33 +0700

    fixed name

--- src/common/admin-protected-route.js
+++ src/common/admin-protected-route.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import AuthService from "../services/auth.service"
 
-const RouteForAdmin = ({ component, ...args }) => {
+const AdminProtectedRoute = ({ component, ...args }) => {
     return AuthService.isAdmin() ? (<Route component={(component)} {...args} />) : (<Redirect to="/"/>)
 };
 
-export default RouteForAdmin;
+export default AdminProtectedRoute;
