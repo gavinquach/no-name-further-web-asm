029913b4dde1131cc378ecbd811fa891c58c72e2
gavinquach <gvqch8@gmail.com>
2021-09-05 20:39:25 +0700

    removed navbar bottom border & fixed footer code

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
 import '../../css/Footer.css'
-
 
 export default class Footer extends Component {
     // Footer component
     render() {
         return (
-            <div className = "main-footer">
-                         {/* Column 1*/}
-                         <div id ="description">
-                            <h4 style={{"margin-bottom": "0px", "margin-top":"0px" }}>RMIT University SGS Campus</h4><br></br>
-                            Further Web Programming Assignment<br></br>
-                            <b>Address: </b>702 Nguyen Van Linh, Tan Hung, District 7<br></br> Ho Chi Minh City, Vietnam<br></br> 
-
-                                    
-                        </div>
-                         {/* Column 2*/}
-                         <div class = "col">
-                            <h4>ABOUT US</h4>                    
-                         </div>
-                     <hr/>
+            <div className="main-footer">
+                {/* Column 1*/}
+                <div id="description">
+                    <h4 style={{ marginBottom: "0px", marginTop: "0px" }}>RMIT University SGS Campus</h4><br></br>
+                    Further Web Programming Assignment<br></br>
+                    <b>Address: </b>702 Nguyen Van Linh, Tan Hung, District 7<br></br> Ho Chi Minh City, Vietnam<br></br>
                 </div>
+                
+                {/* Column 2*/}
+                <div className="col">
+                    <h4>ABOUT US</h4>
+                </div>
+                <hr />
+            </div>
         );
     }
 }
