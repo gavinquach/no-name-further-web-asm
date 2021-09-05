fccba4883e679d9430f796c2b1af096af4e9c2e3
gavinquach <gvqch8@gmail.com>
2021-09-05 20:36:30 +0700

    fixed navbar to top

--- src/components/Navbar/NavigationBar.js
+++ src/components/Navbar/NavigationBar.js
@@ -50,93 +50,96 @@ export default class NavigationBar extends Component {
     render = () => {
         const currentUser = this.props.obj;
         return (
-            <Navbar className="navbar" expand="lg" >
-                <Navbar.Brand href="/"><Image src={logo} fluid style={{ marginLeft: '1em', width: '3em', maxWidth: '3em', height: "100%" }} /></Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className="nav">
-                        <Nav.Link className="navbar-text navbar-item" href="/transactions" >Transactions</Nav.Link>
-                        <Nav.Link className="navbar-text navbar-item" href="/cart">Cart</Nav.Link>
+            <div>
+                <Navbar className="navbar" expand="lg" >
+                    <Navbar.Brand href="/"><Image src={logo} fluid style={{ marginLeft: '1em', width: '3em', maxWidth: '3em', height: "100%" }} /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className="nav">
+                            <Nav.Link className="navbar-text navbar-item" href="/transactions" >Transactions</Nav.Link>
+                            <Nav.Link className="navbar-text navbar-item" href="/cart">Cart</Nav.Link>
 
-                        {/* show user panel user is logged in */}
-                        {currentUser && (
-                            <Nav.Link className="navbar-text navbar-item" href="/user/index">User Panel</Nav.Link>
-                        )}
-                        {/* show admin panel fi user is admin */}
-                        {(currentUser && currentUser.isAdmin) && (
-                            <Nav.Link className="navbar-text navbar-item" href="/admin/index">Admin Panel</Nav.Link>
-                        )}
-                    </Nav>
+                            {/* show user panel user is logged in */}
+                            {currentUser && (
+                                <Nav.Link className="navbar-text navbar-item" href="/user/index">User Panel</Nav.Link>
+                            )}
+                            {/* show admin panel fi user is admin */}
+                            {(currentUser && currentUser.isAdmin) && (
+                                <Nav.Link className="navbar-text navbar-item" href="/admin/index">Admin Panel</Nav.Link>
+                            )}
+                        </Nav>
 
-                    <Nav className="notification">
-                        <div>Notifications</div>
-                        {this.state.notifications.length > 0 &&
-                            <span className="badge">{this.state.notifications.length}</span>
-                        }
-                    </Nav>
-                    <div id="notification-panel">
-                        {/* display if there are notifications in list */}
-                        {this.state.notifications.length > 0 ? (
-                            <div>
-                                {/* loop through to list out the notifications */}
-                                {this.state.notifications.map((notification, index) => (
-                                    <div>
-                                        {/* display up to 5 items */}
-                                        {index < 5 && (
-                                            <Nav.Link href={notification.url} className="notification-items">
-                                                {notification.message}
-                                            </Nav.Link>
-                                        )}
-                                        {/* display "View more" for 6th item */}
-                                        {index == 5 && (
-                                            <Nav.Link href="/notifications" id="notification-view-more">
-                                                View more
-                                            </Nav.Link>
-                                        )}
-                                    </div>
-                                ))}
-                            </div>
-                        ) : (
-                            // no notifications, display text
-                            <div style={{ padding: '1em 2em', textAlign: 'center' }}>
-                                <strong><h5>No Notifications</h5></strong>
-                            </div>
-                        )}
-                    </div>
+                        <Nav className="notification">
+                            <div>Notifications</div>
+                            {this.state.notifications.length > 0 &&
+                                <span className="badge">{this.state.notifications.length}</span>
+                            }
+                        </Nav>
+                        <div id="notification-panel">
+                            {/* display if there are notifications in list */}
+                            {this.state.notifications.length > 0 ? (
+                                <div>
+                                    {/* loop through to list out the notifications */}
+                                    {this.state.notifications.map((notification, index) => (
+                                        <div>
+                                            {/* display up to 5 items */}
+                                            {index < 5 && (
+                                                <Nav.Link href={notification.url} className="notification-items">
+                                                    {notification.message}
+                                                </Nav.Link>
+                                            )}
+                                            {/* display "View more" for 6th item */}
+                                            {index == 5 && (
+                                                <Nav.Link href="/notifications" id="notification-view-more">
+                                                    View more
+                                                </Nav.Link>
+                                            )}
+                                        </div>
+                                    ))}
+                                </div>
+                            ) : (
+                                // no notifications, display text
+                                <div style={{ padding: '1em 2em', textAlign: 'center' }}>
+                                    <strong><h5>No Notifications</h5></strong>
+                                </div>
+                            )}
+                        </div>
 
-                    {/* show username and logout button if logged in, otherwise, show log in and sign up buttons */}
-                    <Nav>
-                        {currentUser ? (
-                            <span className="navbar-item">
-                                <button className="button1">
-                                    <Nav.Link href="/user" id="username-text" className="button-text">
-                                        {currentUser.username}
-                                    </Nav.Link>
-                                </button>
-                            </span>
-                        ) : (
-                            <span className="navbar-item">
-                                <button className="button1">
-                                    <Nav.Link href="/login" className="button-text">Log In</Nav.Link>
-                                </button>
-                            </span>
-                        )}
-                        {currentUser ? (
-                            <span className="navbar-item">
-                                <button className="button2" onClick={this.logOut}>
-                                    <Nav.Link href="/" className="button-text">Log Out</Nav.Link>
-                                </button>
-                            </span>
-                        ) : (
-                            <span className="navbar-item">
-                                <button className="button2">
-                                    <Nav.Link href="/signup" className="button-text">Sign Up</Nav.Link>
-                                </button>
-                            </span>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+                        {/* show username and logout button if logged in, otherwise, show log in and sign up buttons */}
+                        <Nav>
+                            {currentUser ? (
+                                <span className="navbar-item">
+                                    <button className="button1">
+                                        <Nav.Link href="/user" id="username-text" className="button-text">
+                                            {currentUser.username}
+                                        </Nav.Link>
+                                    </button>
+                                </span>
+                            ) : (
+                                <span className="navbar-item">
+                                    <button className="button1">
+                                        <Nav.Link href="/login" className="button-text">Log In</Nav.Link>
+                                    </button>
+                                </span>
+                            )}
+                            {currentUser ? (
+                                <span className="navbar-item">
+                                    <button className="button2" onClick={this.logOut}>
+                                        <Nav.Link href="/" className="button-text">Log Out</Nav.Link>
+                                    </button>
+                                </span>
+                            ) : (
+                                <span className="navbar-item">
+                                    <button className="button2">
+                                        <Nav.Link href="/signup" className="button-text">Sign Up</Nav.Link>
+                                    </button>
+                                </span>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <div id="navbar-spacing"></div>
+            </div>
         );
     }
 }
