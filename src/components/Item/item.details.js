372b3d0393348c1047efe91af33cc405b87e4d4c
gavinquach <gvqch8@gmail.com>
2021-09-12 05:25:16 +0700

    BIG updates to item details page

--- src/components/Item/item.details.js
+++ src/components/Item/item.details.js
@@ -1,56 +1,37 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFrown, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import draftToHtml from 'draftjs-to-html';
+import DOMPurify from 'dompurify';
+
 import AuthService from "../../services/auth.service";
 import UserService from "../../services/user.service";
 import ItemService from "../../services/item.service";
 import TransactionService from "../../services/transaction.service";
 import socket from '../../services/socket';
 
-import { Container, Row, Carousel, Col } from "react-bootstrap";
+import { Row, Carousel, Col } from "react-bootstrap";
 
-import Offers from "./item.offers";
+import PopularOffers from "../popularoffers";
 
 import "../../css/ItemDetails.css"
 
-// const updateQty = (id) => {
-//     let qtyValue = document.getElementById("qty-value").value;
-//     if (!isNaN(qtyValue)) {
-//         Math.round(qtyValue);
-//         if (id === "decrease-qty") {
-//             if (qtyValue > 1) {
-//                 qtyValue--;
-//                 document.getElementById("qty-value").value = qtyValue;
-//             }
-//         } else {
-//             qtyValue++;
-//             document.getElementById("qty-value").value = qtyValue;
-//         }
-//     }
-// }
+// format the date to be readable from Date object
+const formatDate = (d) => {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
 
-const changeImage = (e) => {
-    let item;
-    let img;
-    if (e.target.tagName == "LI") {
-        item = e.target;
-        img = e.target.firstChild.src;
-    }
-    else if (e.target.tagName == "IMG") {
-        item = e.target.parentElement;
-        img = e.target.src;
-    }
+    const dateObj = new Date(d);
+    const date = dateObj.getDate();
+    const month = monthNames[dateObj.getMonth()];   // add leading 0 to month
+    const year = dateObj.getFullYear();
+    const hour = ("0" + dateObj.getHours()).slice(-2);   // add leading 0 to hour
+    const minute = ("0" + (dateObj.getMinutes())).slice(-2);   // add leading 0 to minute
+    const second = ("0" + (dateObj.getSeconds())).slice(-2);
 
-    document.getElementById("main-image").src = img;
-
-    // get elements from class to remove border from
-    let elements = document.getElementsByClassName("image-item");
-
-    // remove border from all images
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.remove("image-list-img-border");
-    }
-    // add border to selected preview image
-    item.classList.add("image-list-img-border");
+    return `${month} ${date}, ${year} at ${hour}:${minute}:${second}`;
 }
 
 export default class ItemDetails extends Component {
@@ -59,18 +40,7 @@ export default class ItemDetails extends Component {
 
         this.state = {
             notfound: false,
-            item: {
-                _id: "",
-                name: "",
-                quantity: 0,
-                type: "",
-                forItemName: "",
-                forItemQty: 0,
-                forItemType: "",
-                images: [],
-                seller: "",
-                offers: 0
-            },
+            item: null,
             images: [],
             successful: false,
             message: ""
@@ -81,25 +51,8 @@ export default class ItemDetails extends Component {
     componentDidMount() {
         ItemService.viewOneItem(this.props.match.params.id)
             .then(response => {
-                if (response.status != 200) {
-                    this.setState({
-                        notfound: true
-                    });
-                }
                 this.setState({
-                    item: {
-                        _id: response.data._id,
-                        name: response.data.name,
-                        quantity: response.data.quantity,
-                        type: response.data.type.name,
-                        forItemName: response.data.forItemName,
-                        forItemQty: response.data.forItemQty,
-                        forItemType: response.data.forItemType.name,
-                        images: response.data.images,
-                        seller: response.data.seller._id,
-                        offers: response.data.offers
-                    },
-                    seller: response.data.seller,
+                    item: response.data
                 }, () => this.addImages());
             })
             .catch((error) => {
@@ -109,7 +62,7 @@ export default class ItemDetails extends Component {
                 if (error.response && error.response.status != 500) {
                     console.log(error.response.data.message);
                 } else {
-                    console.log(error);
+                    console.log(error.response);
                 }
             })
     }
@@ -193,56 +146,128 @@ export default class ItemDetails extends Component {
         );
     }
 
+    chatWithUser = () => {
+        const data = {
+            user: AuthService.getCurrentUser().id,
+            receiver: this.state.item.seller._id
+        };
+        socket.emit("chatWithUserRequest", data);
+    }
+
     render() {
+        const item = this.state.item && this.state.item;
+
+        // parse description to be able to display in HTML
+        let description = null;
+        if (item && item.description) {
+            description = draftToHtml(
+                JSON.parse(item.description)
+            );
+        }
         return (
             <div>
-                {this.state.notfound ? (
-                    <div className="container" style={{ width: '60em', height: '40em' }}>
-                        <h1 style={{ textAlign: 'center', marginTop: '50%' }}>
-                            Something went wrong, we can't find the item :(
-                        </h1>
-                    </div>
-                ) : (
-                    <div className="">
+                {this.state.notfound && (
+                    <span style={{ textAlign: 'center' }}>
                         <Helmet>
-                            <title>Detail Page</title>
+                            <title>404 Item not found</title>
                         </Helmet>
-
-                        <Container>
+                        <h1>
+                            <FontAwesomeIcon id="fa-icon-frown" icon={faFrown} />
+                        </h1>
+                        <h1 id="four-o-four">404</h1>
+                        <h1>Item not found</h1>
+                    </span>
+                )}
+                {item && (
+                    <span>
+                        <Helmet>
+                            <title>Item details</title>
+                        </Helmet>
+                        <div className="ItemDetailsContainer">
                             <div className="title">Item Details</div>
                             <hr className="section-line" />
+                            <Carousel fade>
+                                {item.images.map((image, index) => {
+                                    return (
+                                        <Carousel.Item key={"carousel-item-" + index}>
+                                            <img
+                                                className="CarouselImage"
+                                                src={process.env.REACT_APP_NODEJS_URL.concat("images/", image.name)}
+                                                alt={"Slide" + index}
+                                            />
+                                        </Carousel.Item>
+                                    )
+                                })}
+                            </Carousel>
                             <Row>
-                                <Carousel className="m-5" fade>
-                                    {this.state.item.images.map((image, index) => {
-                                        return (
-                                            <Carousel.Item key={index} className="carousel__item" >
-                                                <img className="d-block w-100 h-100 pic" src={process.env.REACT_APP_NODEJS_URL.concat("images/", image.name)} alt={"Slide" + index} />
-                                            </Carousel.Item>
-                                        )
-                                    })}
-                                </Carousel>
+                                <Col className="UserDetails">
+                                    {/* <img className="avatar-image" src="" alt="User avatar" /> */}
+                                    <FontAwesomeIcon icon={faUserCircle} id="avatar" />
+
+                                    <div className="NameLocation">
+                                        <h2>{item.seller.username}</h2>
+                                        <h4><b>City:</b> {item.seller.location[0].replace("Thành phố ", "").replace("Tỉnh ", "")}</h4>
+                                        <h4><b>District:</b> {item.seller.location[1].replace("Huyện ", "").replace("Quận ", "")}</h4>
+                                    </div>
+                                    <button className="ChatWithUser" onClick={this.chatWithUser}>Chat with user</button>
+                                </Col>
                             </Row>
+                            <hr className="divide" />
+                            <div className="Flexbox-container">
+                                <div style={{ width: '70%' }}>
+                                    <h2 className="SectionHeader">Details:</h2>
+                                    <div id="details-section">
+                                        <div id="date-text-container">
+                                            <div>Upload date: {formatDate(item.upload_date)}</div>
+                                            <div>Last updated: {formatDate(item.last_update)}</div>
+                                        </div>
+                                        <div id="want-to-trade-text">
+                                            I want to trade <b>{item.name}</b> with <b>{item.forItemName}</b>
+                                        </div>
 
-                            <Row>
-                                <Col>
-                                    <div>
-                                        <img className="avatar-detail" src="https://media.dayoftheshirt.com/images/shirts/uuaFB/qwertee_chubby-penguin_1478643177.large.png" alt="Avatar" />
+                                        <br />
 
-                                        <div className="address-name">
-                                            <h3>{this.state.item.name}</h3>
-                                            <h3>Address</h3>
+                                        <div className="TypeQuantityContainer">
+                                            <div style={{ marginRight: '30px' }}>
+                                                <br />
+                                                <div><b>Category:</b></div>
+                                                <div><b>Quantity:</b></div>
+                                            </div>
+                                            <div className="Column">
+                                                My item:
+                                                <div>{item.type.name}</div>
+                                                <div>{item.quantity}</div>
+                                            </div>
+                                            <div className="SeparateLine" />
+                                            <div className="Column">
+                                                Item I want:
+                                                <div>{item.forItemType.name}</div>
+                                                <div>{item.forItemQty}</div>
+                                            </div>
+                                        </div>
+                                        <br />
+                                        <hr />
+                                        <div>
+                                            <h2 className="SectionHeader">Description:</h2>
+                                            <div className="Description">
+                                                {description ? (
+                                                    <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }} />
+                                                ) : (
+                                                    <div>This user doesn't provide any description.</div>
+                                                )}
+                                            </div>
                                         </div>
                                     </div>
-                                </Col>
-                                <Col></Col>
-                                <Col>
-                                    <div className="add-cart-transaction">
-                                        <div>
-                                            <button id="addToCart" className="btn btn-success add-to-cart" onClick={this.addToCart}>Add Cart</button>
-                                        </div>
-                                        <div>
-                                            <button id="makeTransaction" className="btn btn-primary mt-2 make-transaction" onClick={this.makeTransaction}>Request trade</button>
-                                        </div>
+                                </div>
+                                <div className="DetailsRightColumn">
+                                    <div className="ItemStats">
+                                        <p>Views: . Offers: {item.offers}</p>
+                                    </div>
+                                    <div className="ActionButtons">
+                                        <button className="add-to-cart" onClick={this.addToCart}>Add To Cart</button>
+                                        <br />
+                                        <br />
+                                        <button className="request-trade" onClick={this.makeTransaction}>Request trade</button>
                                         {this.state.message && (
                                             <div className="statusMsg">
                                                 <div className={this.state.successful ? "alert alert-success" : "alert alert-danger"} role="alert">
@@ -251,66 +276,15 @@ export default class ItemDetails extends Component {
                                             </div>
                                         )}
                                     </div>
-                                </Col>
-                            </Row>
+                                </div>
+                            </div>
                             <hr className="divide" />
                             <Row>
-                                <div>
-                                    <div className="title">Description</div>
-                                    <hr className="section-line" />
-                                    <Container>
-                                        <div className="description-table">
-                                            <table>
-                                                <tbody>
-                                                    <tr>
-                                                        <td className="description-section">Type:</td>
-                                                        <td className="">{this.state.item.type}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td className="description-section">Quantity:</td>
-                                                        <td>{this.state.item.quantity}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td className="description-section">For item:</td>
-                                                        <td>{this.state.item.forItemName}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td className="description-section">For item type:</td>
-                                                        <td>{this.state.item.forItemType}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td className="description-section">For item quantity:</td>
-                                                        <td>{this.state.item.forItemQty}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </Container>
-                                </div>
+                                <PopularOffers obj={true} />
                             </Row>
-                            <Row>
-                                <div className="ml-3">
-                                    <div>
-                                        <div>
-                                            <div className="user-description">
-                                                <p className="diff-text">I want to trade Dried Fish</p>
-                                                <p className="diff-text ">View 321 Offered 5 Cart 10</p>
-                                            </div>
-                                            <p className="diff-text history-text">Uploaded 2 days ago</p>
-                                        </div>
-                                    </div>
-                                    <div className="diff-text-2">2kg</div>
-                                    <div className="diff-text-2">I'm willing to trade with some vegetable. I can pick up your food if you are in D7</div>
-                                </div>
-                            </Row>
-                            <hr className="divide" />
-                            <Row>
-                                <Offers />
-                            </Row>
-                        </Container>
-                    </div>
-                )
-                }
+                        </div>
+                    </span>
+                )}
             </div>
         );
     }
